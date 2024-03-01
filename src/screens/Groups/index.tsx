54b8eb51515cd5086eb408eab9146569c47e9ed5
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-03-01 18:53:44 -0300

    feat: Componente List Empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,13 +1,14 @@
 import { GroupCard } from '@components/GroupCard'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
+import { ListEmpty } from '@components/ListEmpty'
 import { useState } from 'react'
 import { FlatList } from 'react-native'
 
 import { Container } from './styles'
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Galera da Faculdade', 'Amigos', 'Dota 2'])
+  const [groups, setGroups] = useState<string[]>([])
 
   return (
     <Container>
@@ -19,6 +20,8 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira turma?" />}
       />
     </Container>
   )
