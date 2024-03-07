7a00e1803f34cc13cb45fff7d2e4f2218a8c6204
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-03-06 21:01:04 -0300

    chore: configurando regras import

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,20 @@ module.exports = {
   plugins: ['@typescript-eslint', 'prettier', 'simple-import-sort'],
   extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
   rules: {
-    'simple-import-sort/imports': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          ['^react', '^@?\\w'],
+
+          ['^@components', '^@screens', '^@utils', '^@hooks', '^@store'],
+
+          ['^\\.+'],
+
+          ['^.+\\.css$', '^.+\\.(s(c|a)ss)$', '^.+\\.less$', '^.+\\.styl$', '^.+\\.theme$']
+        ]
+      }
+    ],
     'prettier/prettier': 'error'
   }
 }
