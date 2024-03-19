b5717b646262eb7e060b523abacad2a138c1f8ac
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-03-18 21:48:58 -0300

    feat: Navegando pela aplicação

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { FlatList } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 
 import { Button, GroupCard, Header, Highlight, ListEmpty } from '@components/index'
 
@@ -7,6 +8,12 @@ import { Container } from './styles'
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
+
+  const { navigate } = useNavigation()
+
+  function handleNewGroup() {
+    navigate('new')
+  }
 
   return (
     <Container>
@@ -22,7 +29,7 @@ export function Groups() {
         ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira turma?" />}
       />
 
-      <Button title="Criar nova turma" />
+      <Button title="Criar nova turma" onPress={handleNewGroup} />
     </Container>
   )
 }
