632f8309eb13f5acdb410487a3e2cd6f7fd58e53
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-03-18 22:36:28 -0300

    feat: Passando parâmetros pela rota

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 
 import { Button, Header, Highlight, Input } from '@components/index'
@@ -5,10 +6,12 @@ import { Button, Header, Highlight, Input } from '@components/index'
 import { Container, Content, Icon } from './styles'
 
 export function NewGroup() {
+  const [group, setGroup] = useState('')
+
   const { navigate } = useNavigation()
 
   function handleNew() {
-    navigate('players', { group: 'Novo Grupo' })
+    navigate('players', { group })
   }
   return (
     <Container>
@@ -19,7 +22,7 @@ export function NewGroup() {
 
         <Highlight title="Nova turma" subTitle="Crie a turma para adicionar as pessoas" />
 
-        <Input placeholder="Nome da turma" />
+        <Input placeholder="Nome da turma" onChangeText={setGroup} />
 
         <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { FlatList } from 'react-native'
+import { useRoute } from '@react-navigation/native'
 
 import {
   Button,
@@ -14,15 +15,22 @@ import {
 
 import { Container, Form, HeaderList, NumbersOfPlayers } from './styles'
 
+type RouteParams = {
+  group: string
+}
+
 export function Players() {
   const [team, setTeam] = useState('Time A')
   const [players, setPlayers] = useState(['Jonatán'])
+
+  const { params } = useRoute()
+  const { group } = params as RouteParams
 
   return (
     <Container>
       <Header showBackButton />
 
-      <Highlight title="Nova turma" subTitle="Adicione a galera e separe os times" />
+      <Highlight title={group} subTitle="Adicione a galera e separe os times" />
 
       <Form>
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
