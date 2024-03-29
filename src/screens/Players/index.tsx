7f8e293573f3a7c986a4a723f3af693e39c67bb2
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-03-28 21:58:14 -0300

    feat: Melhorando a usabilidade

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react'
-import { Alert, FlatList } from 'react-native'
+import { useEffect, useRef, useState } from 'react'
+import { Alert, FlatList, TextInput } from 'react-native'
 import { useRoute } from '@react-navigation/native'
 
 import {
@@ -33,6 +33,8 @@ export function Players() {
   const { params } = useRoute()
   const { group } = params as RouteParams
 
+  const newPlayerNameInputRef = useRef<TextInput>(null)
+
   async function handleAddPlayer() {
     if (newPlayerName.trim().length === 0) {
       return Alert.alert('Nova pessoa', 'Informe o nome da pessoa para adicionar.')
@@ -46,6 +48,9 @@ export function Players() {
     try {
       await playerAddByGroup(newPlayer, group)
 
+      newPlayerNameInputRef.current?.blur()
+
+      setNewPlayerName('')
       getPlayersByTeam()
     } catch (error) {
       if (error instanceof AppError) {
@@ -77,7 +82,15 @@ export function Players() {
       <Highlight title={group} subTitle="Adicione a galera e separe os times" />
 
       <Form>
-        <Input onChangeText={setNewPlayerName} placeholder="Nome da pessoa" autoCorrect={false} />
+        <Input
+          inputRef={newPlayerNameInputRef}
+          onChangeText={setNewPlayerName}
+          value={newPlayerName}
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+          onSubmitEditing={handleAddPlayer}
+          returnKeyType="done"
+        />
 
         <ButtonIcon icon="add" onPress={handleAddPlayer} />
       </Form>
